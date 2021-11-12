bb02eace155153886356ddc552d9058a84f20b62
IlBarbiereDiSiviglia <86927816+IlBarbiereDiSiviglia@users.noreply.github.com>
2021-11-12 17:07:08 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,18 @@ const closeAuth = document.querySelector('.close-auth');
 const logInForm = document.getElementById('logInForm');
 const inputLogin = document.getElementById('login');
 const inputPassword = document.getElementById('password');
+const buttonOut = document.querySelector('.button-out');
+const userName = document.querySelector('.user-name');
 
+
+
+const login = (user) => {
+	buttonAuth.style.display = 'none';
+	buttonOut.style.display = 'block';
+	userName.style.display = 'block';
+	userName.textContent = user.login;
+	modalAuth.style.display = 'none';
+}
 
 buttonAuth.addEventListener('click', () => {
 	modalAuth.style.display = 'flex';
@@ -22,5 +33,6 @@ logInForm.addEventListener('submit', () => {
 		password: inputPassword.value,
 	}
 
-	console.log(user);
+	login(user);
 })
+
